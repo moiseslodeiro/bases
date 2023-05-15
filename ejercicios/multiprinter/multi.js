be9782cfc22bd6e6bb9cce2e6ba1def5ab66d358
Moisés Lodeiro <moises.lodeiro@gmail.com>
2023-05-15 23:22:13 +0100

    Añadidos ejercicios de objetos

---
+++ ejercicios/multiprinter/multi.js
@@ -0,0 +1,19 @@
+/**
+ * Vamos a darle una vuelta de tuerca al prints (si no lo has resuelto, ¡es hora!). En este
+ * caso vamos a suponer que la misma impresora puede recibir documentos de varias páginas (1, 2,
+ * 3, 1 gritón, etc.) y que cada página consume una cantidad de colores diferentes (una consumirá
+ * 2 de cyan, 0 de magenta y 5 de amarillo, otra consumirá 1 de cyan, 2 de magenta, 3 de amarillo,
+ * etc.).
+ *
+ * Realiza los cambios necesarios sabiendo que a la función printer se le pasan dos o más argumentos,
+ * donde el primero sigue siendo el cartucho y los restantes, las páginas a imprimir. La función debe
+ * devolver cuántas páginas podrá imprimir con ese cartucho e imprimir un mensaje de error y devolver
+ * -1 en caso de no poder imprimir la totalidad de las páginas que tiene que imprimir.
+ *
+ */
+function printer(cartridge, pages) {
+  // Inicio código
+  // Fin código
+}
+
+module.exports = printer
